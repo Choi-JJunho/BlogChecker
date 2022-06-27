1c9a8d2dedf55e86445bb97cbe3d858c3dc17282
Choi-JJunho <junho5336@gmail.com>
2022-06-27 11:45:51 +0900

    init commit
    init commit

---
+++ src/main/java/challenge/bolgchecker/BolgcheckerApplication.java
@@ -0,0 +1,13 @@
+package challenge.bolgchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolgcheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolgcheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/challenge/bolgchecker/BolgcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.bolgchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolgcheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
